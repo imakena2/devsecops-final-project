eb9ca4073667f5dc719d96b75391708527fe136c
imakena2 <imakena@gmail.com>
2025-07-10 08:58:55 +0300

    App Image

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(3001, () => {
+  console.log('App listening on port 3001');
+});
